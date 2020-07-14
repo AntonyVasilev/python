a3fc8ec1b5622364897982714f5ed34b79f042a8
AntonyVasilev <antony.n.vasilev@gmail.com>
2020-07-14 08:58:56 +0300

    optimized code in max_num_04

--- max_num_04.py
+++ max_num_04.py
@@ -2,32 +2,19 @@
 
 user_number = int(input('Введите целое положительное число: '))
 
+#Проверка корректности ввода
 while user_number < 0:
     print('Вы ввели отрицательное число.')
-    user_number = int(input('Введиете число от 0 до 9: '))
+    user_number = int(input('Введиете целое положительное число: '))
 
+max_num = 0
 digit = user_number
-digits = []
-i = 0
 
-# Создаю список с цифрами из введенного числа
 while digit > 0:
-    digits.append(digit % 10)
-    digit = digit // 10
-    i += 1
-print(digits)
-
-# Сравниваю числа из списка и нахожу наибольшше
-max_num = digits[0]
-# print(max_num)
-i = 1
-# print(len(digits))
-count = len(digits)
-
-while i < count:
-    if digits[i] > max_num:
-        max_num = digits[i]
-    # print(max_num)
-    i += 1
+    digit = user_number % 10
+    user_number = user_number // 10
+    if digit > max_num:
+        max_num = digit
+    #print(max_num)
 
 print(f'Максимальная цифра в введенном числе: {max_num}')
