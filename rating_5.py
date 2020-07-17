89b060ab618bb3af8b30e43355093edb51746bf7
Antony Vasilev <antony.n.vasilev@gmail.com>
2020-07-17 14:16:21 +0300

    update rating_5

--- rating_5.py
+++ rating_5.py
@@ -13,8 +13,12 @@ while new_number <= 0:
     except ValueError:
         print('Вы ввели не целое число.')
 
-print(new_number)
+for i in range(len(my_list)):
+    if new_number > my_list[i]:
+        my_list.insert(i, new_number)
+        break
+    elif new_number <= my_list[len(my_list) - 1]:
+        my_list.append(new_number)
+        break
 
-my_list.append(new_number)
-my_list.sort(reverse=True)
 print(my_list)
